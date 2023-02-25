84011233066e7e09d431ff8bbb2968f38091c845
ginoagostinelli <ginoagostinelli5@gmail.com>
2023-02-25 20:10:39 -0300

    add error message

--- companyview/routes/errors.py
+++ companyview/routes/errors.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint, Response
+from flask import jsonify, Blueprint, Response, render_template
 
 from ..models.exceptions import (
     CompanyNotValid,
@@ -19,14 +19,14 @@ def __generate_error_response(error: Exception) -> Response:
 def handle_company_not_found(error: CompanyNotValid) -> Response:
     response = __generate_error_response(error)
     response.status_code = 404
-    return response
+    return render_template("error.html", error_message=error)
 
 
 @errors_scope.app_errorhandler(UserNotFound)
 def handle_user_not_found(error: UserNotFound) -> Response:
     response = __generate_error_response(error)
     response.status_code = 404
-    return response
+    return render_template("error.html", error_message=error)
 
 
 @errors_scope.app_errorhandler(UserNotValid)
@@ -34,11 +34,17 @@ def handle_user_not_found(error: UserNotFound) -> Response:
 def handle_other_user_exceptions(error: Exception) -> Response:
     response = __generate_error_response(error)
     response.status_code = 409
-    return response
+    return render_template("error.html", error_message=error)
 
 
 @errors_scope.app_errorhandler(404)
 def handle_not_found(error) -> Response:
     response = __generate_error_response(error)
     response.status_code = 404
-    return response
+    return render_template("error.html", error_message=error)
+
+
+@errors_scope.app_errorhandler(Exception)
+def handle_error(error):
+    # Render a custom error page
+    return render_template("error.html", error_message=error)
